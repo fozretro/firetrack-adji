efe81ae0e0ef95f7e638a0f33725a7f311196cdf
Andrew Fawcett <106936946+fozretro@users.noreply.github.com>
2024-01-28 19:35:48 -0800

    Proof of concept to override default keys

--- src/firetrack-adji.asm
+++ src/firetrack-adji.asm
@@ -4,9 +4,36 @@ ORG &1900
 
 .start
 .exec
-      LDA #66
-      JSR &FFEE
+      LDA &F4
+      PHA
+      LDA #7
+      STA &FE30
+      STA &F4
+
+      LDX #0
+.loop 
+      LDA config,X
+      STA &B100,X
+      INX
+      CPX #0
+      BNE loop
+
+      PLA
+      STA &FE30
+      STA &F4
       RTS
+
+      ALIGN &100
+.config 
+      EQUS "** FireTrack ** by Orlando.(C) Aardvark Software 1987."
+      EQUB 0,0,0,0,0,0,0,0,0,0
+      EQUB 255
+      EQUS "KEYS"
+      EQUB -97
+      EQUB -17
+      EQUB -87
+      EQUB -56
+      EQUB -72
 .end
  
 PUTFILE "dev/firetrack-adji/!BOOT", "!BOOT", &0000
