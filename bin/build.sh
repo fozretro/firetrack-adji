cbaf10bc1d27c717a129d1eb1332826c759bb425
Andrew Fawcett <106936946+fozretro@users.noreply.github.com>
2024-02-03 21:42:03 -0800

    Add boot options file

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,5 @@
 # Compile ./dist/gundroid.ssd
-beebasm -v -i ./src/firetrack-adji.asm -do ./dist/firetrack-adji.ssd -title FTADJI -opt 0
+beebasm -v -i ./src/firetrack-adji.asm -do ./dist/firetrack-adji.ssd -title FTADJI -opt 3
 # Expand it back out into the Beebs development workspace the OBJECT file for local running and to update .inf file for OBJECT
 rm -rf ./dev/firetrack-adji-dist
 perl ./bin/mmbutils/beeb getfile ./dist/firetrack-adji.ssd ./dev/firetrack-adji-dist
